47cab5e709ec904bb5fa8041967a8e38194e6ade
Martin <mpolojko@gmail.com>
2012-07-27 18:42:43 +0200

    Initial commit

---
+++ grails-app/conf/DataSource.groovy
@@ -0,0 +1,33 @@
+// environment specific settings
+environments {
+    development {
+        dataSource {
+            host = "http://192.168.1.66"
+            port = 28017
+            databaseName = "vento"
+        }
+    }
+    test {
+        dataSource {
+            dbCreate = "update"
+            url = "jdbc:h2:mem:testDb;MVCC=TRUE"
+        }
+    }
+    production {
+        dataSource {
+            dbCreate = "update"
+            url = "jdbc:h2:prodDb;MVCC=TRUE"
+            pooled = true
+            properties {
+               maxActive = -1
+               minEvictableIdleTimeMillis=1800000
+               timeBetweenEvictionRunsMillis=1800000
+               numTestsPerEvictionRun=3
+               testOnBorrow=true
+               testWhileIdle=true
+               testOnReturn=true
+               validationQuery="SELECT 1"
+            }
+        }
+    }
+}
---
+++ grails-app/domain/vento/reports/Twit.groovy
@@ -0,0 +1,26 @@
+package vento.reports
+
+public class Twit {
+    String id;
+
+    String text;
+    String geo;
+
+    String toUserIdStr;
+    String source;
+
+    String isoLanguageCode;
+
+    String fromUserIdStr;
+    String query;
+
+    String fromUser;
+    String score;
+
+    String referenceScore;
+
+    String createdAt;
+
+    static constraints = {
+    }
+}
