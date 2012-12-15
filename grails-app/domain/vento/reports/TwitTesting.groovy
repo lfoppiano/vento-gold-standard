f64fbf832e2a0d99609b7f4d05a8b1ed803cdb70
lfoppiano <luca@foppiano.org>
2012-12-15 13:10:38 +0100

    fixed domain classes

---
+++ grails-app/domain/vento/reports/TwitTesting.groovy
@@ -0,0 +1,49 @@
+package vento.reports
+
+import org.bson.types.ObjectId
+
+class TwitTesting {
+    ObjectId id
+
+    String twitterId
+
+    String text;
+    String geo;
+
+    String toUserIdStr;
+    String source;
+
+    String isoLanguageCode;
+
+    String fromUserIdStr;
+    String query;
+
+    String fromUser;
+    String score;
+
+    String referenceScore;
+
+    String createdAt;
+
+    //static hasMany = [scores: Score]
+
+    static constraints = {
+        geo nullable: true
+        toUserIdStr nullable: true
+        source nullable: true
+        isoLanguageCode nullable: true
+        fromUserIdStr nullable: true
+        fromUser nullable: true
+        score nullable: true
+        referenceScore nullable: true
+        createdAt nullable: true
+    }
+
+    static mapWith = "mongo"
+
+    static mapping = {
+        collection "testing"
+        database "vento"
+        //cache true
+    }
+}
